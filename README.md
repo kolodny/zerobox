0449b09a6ce647b5daf2dc27ac4a321bed767475
Moshe Kolodny <moshe@moshe-linux.(none)>
2013-05-28 10:53:11 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 zerobox
 =======
+
+## Usage:
+
+HTML:
+
+    <a class="zerobox" href="image.jpg">
+        <img src="image-thumb.jpg" />
+    </a>
+
+JS:
+   
+    $('a.zerobox').zerobox();
---
+++ zerobox.js
@@ -0,0 +1,45 @@
+$.fn.zerobox = function(options) {
+	options = $.extend({
+		preLoad: true
+	}, options);
+	
+	return this.each(function() {
+		var $this = $(this);
+		
+		$this.click(function(e) {
+			e.preventDefault();
+
+			var $img = $this.find('img'),
+				imgSrc = this.href,
+				offset = $img.offset();
+
+			var $zerobox = $('<div id="zerobox" title="click to close">')
+				.css({
+					position: 'fixed',
+					top: offset.top - $(window).scrollTop(),
+					left: offset.left - $(window).scrollLeft(),
+					height: $img.height(),
+					width: $img.width(),
+					zIndex: 1099,
+					background: 'rgba(0, 0, 0, .75) url("' + imgSrc + '")',
+					backgroundRepeat: 'no-repeat',
+					backgroundPosition: 'center',
+					cursor: 'pointer',
+					textAlign: 'center'
+				})
+				.click(function() { $zerobox.remove(); })
+				.appendTo(document.body).animate({
+					left: 0, top: 0, width: '100%', height: '100%'
+				});
+
+			return false;
+		});
+		if (options.preLoad) {
+			$this.one('mouseover', function() {
+				var image = new Image();
+
+				image.src = this.href; 
+			});
+		}
+	});
+};
