fa37d0b90e453f3a3369cce3ec4c1b31d8e34092
Moshe Kolodny <moshe@moshe-linux.(none)>
2013-05-28 12:51:00 -0400

    changed tabs to spaces and added image animation (yay front page)

--- zerobox.js
+++ zerobox.js
@@ -1,45 +1,55 @@
 $.fn.zerobox = function(options) {
-	options = $.extend({
-		preLoad: true
-	}, options);
-	
-	return this.each(function() {
-		var $this = $(this);
-		
-		$this.click(function(e) {
-			e.preventDefault();
+    options = $.extend({
+        preLoad: true
+    }, options);
+    
+    return this.each(function() {
+        var $this = $(this);
+        
+        $this.click(function(e) {
+            e.preventDefault();
 
-			var $img = $this.find('img'),
-				imgSrc = this.href,
-				offset = $img.offset();
+            var $img = $this.find('img'),
+                imgSrc = this.href,
+                offset = $img.offset(),
+                clonedImg = $img[0].cloneNode(),
+                imgWidth = $img.width(),
+                imgHeight = $img.height(),
+                imgFullWidth = clonedImg.width,
+                imgFullHeight = clonedImg.height;
 
-			var $zerobox = $('<div id="zerobox" title="click to close">')
-				.css({
-					position: 'fixed',
-					top: offset.top - $(window).scrollTop(),
-					left: offset.left - $(window).scrollLeft(),
-					height: $img.height(),
-					width: $img.width(),
-					zIndex: 1099,
-					background: 'rgba(0, 0, 0, .75) url("' + imgSrc + '")',
-					backgroundRepeat: 'no-repeat',
-					backgroundPosition: 'center',
-					cursor: 'pointer',
-					textAlign: 'center'
-				})
-				.click(function() { $zerobox.remove(); })
-				.appendTo(document.body).animate({
-					left: 0, top: 0, width: '100%', height: '100%'
-				});
+            var $zerobox = $('<div id="zerobox" title="click to close">')
+                .css({
+                    position: 'fixed',
+                    top: offset.top - $(window).scrollTop(),
+                    left: offset.left - $(window).scrollLeft(),
+                    height: $img.height(),
+                    width: $img.width(),
+                    zIndex: 1099,
+                    background: 'rgba(0, 0, 0, .75) url("' + imgSrc + '")',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center',
+                    backgroundSize: imgWidth + 'px ' + imgHeight + 'px',
+                    cursor: 'pointer',
+                    textAlign: 'center'
+                })
+                .click(function() { $zerobox.remove(); })
+                .appendTo(document.body).animate({
+                    left: 0, top: 0, width: '100%', height: '100%'
+                });
+                
+            var $animatedDiv = $('<div>').css({width: imgWidth, height: imgHeight});
+            $animatedDiv.animate({width : imgFullWidth, height: imgFullHeight }, {step: function() {
+                $zerobox.css('backgroundSize', $animatedDiv.width() + 'px ' + $animatedDiv.height() + 'px');
+            }});
+        });
+        if (options.preLoad) {
+            $this.one('mouseover', function() {
+                // preload image
+                var image = new Image();
 
-			return false;
-		});
-		if (options.preLoad) {
-			$this.one('mouseover', function() {
-				var image = new Image();
-
-				image.src = this.href; 
-			});
-		}
-	});
+                image.src = this.href; 
+            });
+        }
+    });
 };
