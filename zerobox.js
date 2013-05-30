bf556eb0b18504c14949d08088f57d258a6df2f1
Moshe Kolodny <moshe@moshe-linux.(none)>
2013-05-30 10:42:51 -0400

    the aim of this project is to keep the file as small as possible

--- zerobox.js
+++ zerobox.js
@@ -1,29 +1,4 @@
-/**
- * Zerobox - Lightweight lightbox plugin for jQuery
- *
- * @link https://github.com/kolodny/zerobox
- */
-
-/*
- * Copyright (C) 2012 Moshe Kolodny
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+// more info @ https://github.com/kolodny/zerobox/
 
 ;(function (factory) {
 	if (typeof define === 'function' && define.amd) {
@@ -33,18 +8,6 @@
 		factory(jQuery);
 	}
 }(function ($) {
-
-    /**
-     * Renders the linked image in a lightbox.
-     *
-     * @param    {object}  options              Options
-     * @param    {boolean} options.preLoad      If TRUE, preloads any linked images
-     * @param    {string}  options.closeTooltip Tooltip text displayed when hovering over the background
-     * @return   this
-     * @class    zerobox
-     * @memberOf jQuery.fn
-     */
-
     $.fn.zerobox = function(options) {
         options = $.extend({
             preLoad: true,
