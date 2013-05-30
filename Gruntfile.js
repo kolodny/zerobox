2584e70dabc277d4bb3cfb5284f07f8c53507c9d
Jukka Svahn <web@rahinaa.biz>
2013-05-30 15:22:02 +0300

    Add Grunt tasks for building and JShinting.

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-bumpup');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> | Copyright (C) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> | <%= pkg.homepage %> | Released under the MIT License */\n',
+                report: "gzip"
+            },
+            dist: {
+                files: {
+                    'dist/zerobox.min.js': ['zerobox.js']
+                }
+            }
+        },
+
+        copy: {
+            main: {
+                files: [
+                    {
+                        src  : ['dist/zerobox.min.js'],
+                        dest : 'dist/zerobox.v<%= pkg.version %>.min.js'
+                    }
+                ]
+            }
+        },
+
+        jshint: {
+            files : ['Gruntfile.js', 'zerobox.js'],
+            options : {
+                globals : {
+                    jQuery : true
+                }
+            }
+        },
+
+        bumpup : {
+            files : ['package.json', 'bower.json']
+        }
+    });
+
+    grunt.registerTask('updatePackage', function () {
+        grunt.config.set('pkg', grunt.file.readJSON('package.json'));
+    });
+
+    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('build', ['uglify', 'copy']);
+    grunt.registerTask('default', ['test', 'build']);
+
+    grunt.registerTask('release', function (type) {
+        if (!type) {
+            type = 'patch';
+        }
+
+        grunt.task.run('test');
+        grunt.task.run('bumpup:' + type);
+        grunt.task.run('updatePackage');
+        grunt.task.run('build');
+    });
+};
